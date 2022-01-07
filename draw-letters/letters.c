4d42c183bb86218eaf56c6d7d86f1c7824f3f94c
tuana9a <tuana9a@gmail.com>
2022-01-07 18:07:15 +0700

    change draw letter to camel case

--- draw-letters/letters.c
+++ draw-letters/letters.c
@@ -1,6 +1,6 @@
 #include<ctype.h>
 
-void print_A(int h)
+void printA(int h)
 {
 	if (h == 1) printf("  a    ");
 	if (h == 2) printf(" a a   ");
@@ -8,7 +8,7 @@ void print_A(int h)
 	if (h == 4) printf("a   a  ");
 	if (h == 5) printf("a   a  ");
 }
-void print_B(int h)
+void printB(int h)
 {
 	if (h == 1) printf("bbbb   ");
 	if (h == 2) printf("b  b   ");
@@ -16,7 +16,7 @@ void print_B(int h)
 	if (h == 4) printf("b   b  ");
 	if (h == 5) printf("bbbbb  ");
 }
-void print_C(int h)
+void printC(int h)
 {
 	if (h == 1) printf(" cccc  ");
 	if (h == 2) printf("c      ");
@@ -24,7 +24,7 @@ void print_C(int h)
 	if (h == 4) printf("c      ");
 	if (h == 5) printf(" cccc  ");
 }
-void print_D(int h)
+void printD(int h)
 {
 	if (h == 1) printf("dddd  ");
 	if (h == 2) printf("d   d ");
@@ -32,7 +32,7 @@ void print_D(int h)
 	if (h == 4) printf("d   d ");
 	if (h == 5) printf("dddd  ");
 }
-void print_E(int h)
+void printE(int h)
 {
 	if (h == 1) printf("eeeee  ");
 	if (h == 2) printf("e      ");
@@ -40,7 +40,7 @@ void print_E(int h)
 	if (h == 4) printf("e      ");
 	if (h == 5) printf("eeeee  ");
 }
-void print_F(int h)
+void printF(int h)
 {
 	if (h == 1) printf("fffff ");
 	if (h == 2) printf("f     ");
@@ -48,7 +48,7 @@ void print_F(int h)
 	if (h == 4) printf("f     ");
 	if (h == 5) printf("f     ");
 }
-void print_G(int h)
+void printG(int h)
 {
 	if (h == 1) printf("ggggg  ");
 	if (h == 2) printf("g      ");
@@ -56,7 +56,7 @@ void print_G(int h)
 	if (h == 4) printf("g   g  ");
 	if (h == 5) printf("ggggg  ");
 }
-void print_H(int h)
+void printH(int h)
 {
 	if (h == 1) printf("h   h ");
 	if (h == 2) printf("h   h ");
@@ -64,7 +64,7 @@ void print_H(int h)
 	if (h == 4) printf("h   h ");
 	if (h == 5) printf("h   h ");
 }
-void print_I(int h)
+void printI(int h)
 {
 	if (h == 1) printf("iiiii ");
 	if (h == 2) printf("  i   ");
@@ -72,7 +72,7 @@ void print_I(int h)
 	if (h == 4) printf("  i   ");
 	if (h == 5) printf("iiiii ");
 }
-void print_J(int h)
+void printJ(int h)
 {
 	if (h == 1) printf(" jjjjj ");
 	if (h == 2) printf("   j   ");
@@ -80,7 +80,7 @@ void print_J(int h)
 	if (h == 4) printf("j  j   ");
 	if (h == 5) printf(" jj    ");
 }
-void print_K(int h)
+void printK(int h)
 {
 	if (h == 1) printf("k   k ");
 	if (h == 2) printf("k  k  ");
@@ -88,7 +88,7 @@ void print_K(int h)
 	if (h == 4) printf("k  k  ");
 	if (h == 5) printf("k   k ");
 }
-void print_L(int h)
+void printL(int h)
 {
 	if (h == 1) printf("l     ");
 	if (h == 2) printf("l     ");
@@ -96,7 +96,7 @@ void print_L(int h)
 	if (h == 4) printf("l     ");
 	if (h == 5) printf("lllll ");
 }
-void print_M(int h)
+void printM(int h)
 {
 	if (h == 1) printf("m     m  ");
 	if (h == 2) printf("m m m m  ");
@@ -104,7 +104,7 @@ void print_M(int h)
 	if (h == 4) printf("m     m  ");
 	if (h == 5) printf("m     m  ");
 }
-void print_N(int h)
+void printN(int h)
 {
 	if (h == 1) printf("n   n  ");
 	if (h == 2) printf("nn  n  ");
@@ -112,7 +112,7 @@ void print_N(int h)
 	if (h == 4) printf("n  nn  ");
 	if (h == 5) printf("n   n  ");
 }
-void print_O(int h)
+void printO(int h)
 {
 	if (h == 1) printf(" ooo  ");
 	if (h == 2) printf("o   o ");
@@ -120,7 +120,7 @@ void print_O(int h)
 	if (h == 4) printf("o   o ");
 	if (h == 5) printf(" ooo  ");
 }
-void print_P(int h)
+void printP(int h)
 {
 	if (h == 1) printf("pppp  ");
 	if (h == 2) printf("p   p ");
@@ -128,7 +128,7 @@ void print_P(int h)
 	if (h == 4) printf("p     ");
 	if (h == 5) printf("p     ");
 }
-void print_Q(int h)
+void printQ(int h)
 {
 	if (h == 1) printf(" qqqq   ");
 	if (h == 2) printf("q    q  ");
@@ -136,7 +136,7 @@ void print_Q(int h)
 	if (h == 4) printf("q    q  ");
 	if (h == 5) printf(" qqqq q ");
 }
-void print_R(int h)
+void printR(int h)
 {
 	if (h == 1) printf("rrrr   ");
 	if (h == 2) printf("r   r  ");
@@ -144,7 +144,7 @@ void print_R(int h)
 	if (h == 4) printf("r   r  ");
 	if (h == 5) printf("r   r  ");
 }
-void print_S(int h)
+void printS(int h)
 {
 	if (h == 1) printf(" ssss  ");
 	if (h == 2) printf("s      ");
@@ -152,7 +152,7 @@ void print_S(int h)
 	if (h == 4) printf("    s  ");
 	if (h == 5) printf("ssss   ");
 }
-void print_T(int h)
+void printT(int h)
 {
 	if (h == 1) printf("ttttt ");
 	if (h == 2) printf("  t   ");
@@ -160,7 +160,7 @@ void print_T(int h)
 	if (h == 4) printf("  t   ");
 	if (h == 5) printf("  t   ");
 }
-void print_U(int h)
+void printU(int h)
 {
 	if (h == 1) printf("u   u  ");
 	if (h == 2) printf("u   u  ");
@@ -168,7 +168,7 @@ void print_U(int h)
 	if (h == 4) printf("u   u  ");
 	if (h == 5) printf(" uuu   ");
 }
-void print_V(int h)
+void printV(int h)
 {
 	if (h == 1) printf("v     v  ");
 	if (h == 2) printf("v     v  ");
@@ -176,7 +176,7 @@ void print_V(int h)
 	if (h == 4) printf("  v v    ");
 	if (h == 5) printf("   v     ");
 }
-void print_W(int h)
+void printW(int h)
 {
 	if (h == 1) printf("w    w    w  ");
 	if (h == 2) printf("w   w w   w  ");
@@ -184,7 +184,7 @@ void print_W(int h)
 	if (h == 4) printf(" w w   w w   ");
 	if (h == 5) printf("  w     w    ");
 }
-void print_X(int h)
+void printX(int h)
 {
 	if (h == 1) printf("x   x ");
 	if (h == 2) printf(" x x  ");
@@ -192,7 +192,7 @@ void print_X(int h)
 	if (h == 4) printf(" x x  ");
 	if (h == 5) printf("x   x ");
 }
-void print_Y(int h)
+void printY(int h)
 {
 	if (h == 1) printf("y   y ");
 	if (h == 2) printf(" y y  ");
@@ -200,7 +200,7 @@ void print_Y(int h)
 	if (h == 4) printf("  y   ");
 	if (h == 5) printf("  y   ");
 }
-void print_Z(int h)
+void printZ(int h)
 {
 	if (h == 1) printf("zzzzz ");
 	if (h == 2) printf("   z  ");
@@ -208,7 +208,7 @@ void print_Z(int h)
 	if (h == 4) printf(" z    ");
 	if (h == 5) printf("zzzzz ");
 }
-void print_space(int h)
+void printSpace(int h)
 {
 	if (h == 1) printf("     ");
 	if (h == 2) printf("     ");
--- draw-letters/main.c
+++ draw-letters/main.c
@@ -6,33 +6,33 @@ void print(char* s) {
 	for (int j = 1;j <= 5;++j) {
 		for (int i = 0;i < strlen(s);++i) {
 			switch (s[i]) {
-			case 'a':print_A(j);break;
-			case 'b':print_B(j);break;
-			case 'c':print_C(j);break;
-			case 'd':print_D(j);break;
-			case 'e':print_E(j);break;
-			case 'f':print_F(j);break;
-			case 'g':print_G(j);break;
-			case 'h':print_H(j);break;
-			case 'i':print_I(j);break;
-			case 'j':print_J(j);break;
-			case 'k':print_K(j);break;
-			case 'l':print_L(j);break;
-			case 'm':print_M(j);break;
-			case 'n':print_N(j);break;
-			case 'o':print_O(j);break;
-			case 'p':print_P(j);break;
-			case 'q':print_Q(j);break;
-			case 'r':print_R(j);break;
-			case 's':print_S(j);break;
-			case 't':print_T(j);break;
-			case 'u':print_U(j);break;
-			case 'v':print_V(j);break;
-			case 'w':print_W(j);break;
-			case 'x':print_X(j);break;
-			case 'y':print_Y(j);break;
-			case 'z':print_Z(j);break;
-			case ' ':print_space(j);break;
+			case 'a':printA(j);break;
+			case 'b':printB(j);break;
+			case 'c':printC(j);break;
+			case 'd':printD(j);break;
+			case 'e':printE(j);break;
+			case 'f':printF(j);break;
+			case 'g':printG(j);break;
+			case 'h':printH(j);break;
+			case 'i':printI(j);break;
+			case 'j':printJ(j);break;
+			case 'k':printK(j);break;
+			case 'l':printL(j);break;
+			case 'm':printM(j);break;
+			case 'n':printN(j);break;
+			case 'o':printO(j);break;
+			case 'p':printP(j);break;
+			case 'q':printQ(j);break;
+			case 'r':printR(j);break;
+			case 's':printS(j);break;
+			case 't':printT(j);break;
+			case 'u':printU(j);break;
+			case 'v':printV(j);break;
+			case 'w':printW(j);break;
+			case 'x':printX(j);break;
+			case 'y':printY(j);break;
+			case 'z':printZ(j);break;
+			case ' ':printSpace(j);break;
 			}
 			if (i == strlen(s) - 1) {
 				printf("\n");
